540bfbce0b73f868f73d02adbf0a48905d044559
lushan2866 <lushan2866@gmail.com>
2016-01-20 10:15:11 +0800

    读取网页学习

---
+++ learning.py
@@ -0,0 +1,12 @@
+import urllib
+import urllib.request
+
+data ={}
+data['word']= 'python scrapy'
+url_values=urllib.parse.urlencode(data)
+url= "http://www.baidu.com/s?"
+full_url = url+url_values
+
+data = urllib.request.urlopen(full_url).read()
+data= data.decode('utf-8')
+print(data)
