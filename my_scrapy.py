b2c4ed572f022001408178580090afd8ffe161c1
lushan2866 <lushan2866@gmail.com>
2016-01-22 10:47:49 +0800

    爬虫练习第一关-代码
    完善代码，补充输出信息

--- my_scrapy.py
+++ my_scrapy.py
@@ -10,22 +10,25 @@ visited = set() #已爬取的合集
 url_1 = 'http://www.heibanke.com/lesson/crawler_ex00/'  # 入口页面
 url = url_1+queue[0]
 #print(url) #test
+s=0
 
 while queue:
     the_num = queue.popleft()
     #print(the_num) #test
     the_url = url_1+the_num #将找到数字加入到下一步要打开的网址里
-    print(the_url) #打印测试
-    try: #尝试抓取，如果错误则赞同3s重试
+    #print(the_url) #打印测试
+    try: #尝试抓取，如果错误则暂停3s重试
       data = urllib.request.urlopen(the_url).read()
     except:
       time.sleep(3)
+      #print(e)
       data = urllib.request.urlopen(the_url).read()
 
     data= data.decode('utf-8')
     #print(data)
     linkre = re.compile('\d{5}') #正则表达式
-    print(linkre.findall(data))
+    #print(linkre.findall(data))
     x = linkre.findall(data)[1]
-    print(x)
+    s=s+1
+    print('第'+str(s)+'关的数字'+x)
     queue.append(x)
