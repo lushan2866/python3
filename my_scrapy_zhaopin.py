1db44f1f1c75e79a4b51f0e48327629d62adde5c
lushan2866 <lushan2866@gmail.com>
2016-03-04 16:38:37 +0800

    新增函数2

--- my_scrapy_zhaopin.py
+++ my_scrapy_zhaopin.py
@@ -1,16 +1,37 @@
 __author__ = 'Ls'
 # -*- coding:utf-8 -*-
-import urllib,urllib.request,urllib.parse,re
+import urllib,urllib.request,urllib.parse,re,time
+page= '1'
+sou_url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E5%8C%97%E4%BA%AC&kw=%E7%BC%96%E8%BE%91&p='+page+'&isadv=0'
+visited=[]
+job_url=[]
 
-url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?jl=%E5%8C%97%E4%BA%AC&kw=%E7%BC%96%E8%BE%91&p=1&isadv=0'
-
-x= urllib.request.urlopen(url).read().decode('utf-8')
-#print(x)
-
-#利用正则表达式获取网址
-def find_out(the_read_url):
-    linkre = re.compile('"http://jobs.zhaopin.com/.\d*?.htm"')# 正则表达式，需要每次不同时候都调整
-    x=linkre.findall(the_read_url)
+#######通过request获取网页原始内容######
+def read_html(url):
+    print('正在抓取网页')
+    try:
+        x = urllib.request.urlopen(url).read().decode('utf-8')
+    except Exception as e:
+        print('抓取错误，输入错误代码，再次尝试')
+        print(e)
+        time.sleep(3)
+        x = urllib.request.urlopen(url).read().decode('utf-8')
     return x
 
-print(find_out(x))
+#######利用正则表达式获取抓取网址的，并判断是否已经爬取过######
+def find_out(the_read_url):
+    linkre = re.compile('http://jobs.zhaopin.com/.\d*?.htm')# 正则表达式，需要每次不同时候都调整
+    #x=linkre.findall(the_read_url)
+    #return x
+    for x in linkre.findall(the_read_url):
+        if  x not in visited:
+         job_url.append(x)
+         print('加入队列 --->  ' + x)
+
+def find_job():
+
+find_out(read_html(sou_url))
+print(job_url)
+
+
+
